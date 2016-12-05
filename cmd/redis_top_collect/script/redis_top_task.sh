ab32f0266a2a6b62a4f79d8d32a2806b222ee047
tangguangchao <tangguangchao@wanda.cn>
2016-12-05 13:35:15 +0800

    调整为30s一次采集

--- cmd/redis_top_collect/script/redis_top_task.sh
+++ cmd/redis_top_collect/script/redis_top_task.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup /opt/stats/bin/redis_top_collect -h 10.209.16.113 -p 8086 -d redis -n 12 >> /dev/null 2>&1 &
+nohup /opt/stats/bin/redis_top_collect -h 10.209.16.113 -p 8086 -d redis -n 2 >> /dev/null 2>&1 &
