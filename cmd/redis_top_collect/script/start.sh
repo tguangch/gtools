bc66b4777ef5ca5b46325f8e83ad9055718b9f89
tangguangchao <tangguangchao@wanda.cn>
2016-12-05 12:55:33 +0800

    获取ip列表

--- cmd/redis_top_collect/script/start.sh
+++ cmd/redis_top_collect/script/start.sh
@@ -13,7 +13,7 @@ DIR_HOME=`pwd`
 BASEDIR=$DIR_HOME/..
 
 echo "#!/bin/sh" > $DIR_HOME/redis_top_task.sh
-echo "nohup $DIR_HOME/../bin/redis_top_collect -h 10.209.16.113 -p 8086 -d redis -n 12 >> /dev/null 2>&1 &" >> $DIR_HOME/redis_top_task.sh
+echo "nohup $DIR_HOME/../bin/redis_top_collect -h 10.209.16.113 -p 8086 -d redis -n 2 >> /dev/null 2>&1 &" >> $DIR_HOME/redis_top_task.sh
 
 echo "* * * * * $DIR_HOME/redis_top_task.sh" >> /var/spool/cron/root
 #service crond restart
